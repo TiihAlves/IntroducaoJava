cecee02ccdf4af4bdc2b3c0b466c7a06af29fd55
TiihAlves <tiih.alves1@gmail.com>
2024-10-28 11:43:25 -0300

    Introducao Operadores

---
+++ src/operadores/Aritimeticos.java
@@ -0,0 +1,19 @@
+package operadores;
+
+public class Aritimeticos {
+
+	public static void main(String[] args) {
+		int x = 4;
+		int y = 2;
+		int a = 3;
+		int res;
+		
+		res = x *(a + y);
+		x += y;
+		
+		System.out.println("Resultado é: "+ res);
+		
+		
+	}
+
+}
---
+++ src/variaveis/VariaveisConstantes.java
@@ -0,0 +1,22 @@
+package variaveis;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		byte bit = 127; 
+		short valor = 32767;
+		int quantidade = 2147483647;
+		long identificador = 9223372036854775807L ;
+		float altura = 1.62F; //16 bits
+		double area = 2.5; //32 bits
+		char tipo = 'A'; // 8 bits
+		boolean resposta = true;
+		
+		final int QUANTIDADE = 1000;
+		
+		String TEXTO = "Generation Brasil!";
+		
+	}
+
+}
